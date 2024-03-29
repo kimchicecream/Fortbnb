7a9832ad6749c7749b75d80fbc03421ba9674987
Alex <gohalexx@gmail.com>
2024-03-29 15:37:12 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,9 @@ const router = express.Router();
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 // LOGIN
 router.post('/', async (req, res, next) => {
     const { credential, password } = req.body;
@@ -59,6 +62,51 @@ router.get('/', (req, res) => {
             user: safeUser
         });
     } else return res.json({ user: null });
+});
+
+// VALIDATE LOGIN
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+router.post('/', validateLogin, async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.unscoped().findOne({
+        where: {
+            [Op.or]: {
+                username: credential,
+                email: credential
+            }
+        }
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+    }
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+        user: safeUser
+    });
 });
 
 module.exports = router;
