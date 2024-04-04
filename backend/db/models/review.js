f452890bd4c0e5910c1bace25b509554e64c5026
Alex <gohalexx@gmail.com>
2024-04-04 08:47:45 -0700

    committed for testing

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -42,8 +42,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
-    stars: DataTypes.INTEGER,
-    avgRating: DataTypes.FLOAT
+    stars: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Review',
