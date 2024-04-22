d25cf2e951fad9385b445710333b83290e12691a
Alex <gohalexx@gmail.com>
2024-04-22 08:36:49 -0700

    Testing 2

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton';
@@ -9,38 +9,58 @@ import './Navigation.css';
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
-  const sessionLinks = sessionUser ?
-    (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    ) : (
-      <>
-        <li>
+  // const sessionLinks = sessionUser ?
+  //   (
+  //     <li>
+  //       <ProfileButton user={sessionUser} />
+  //     </li>
+  //   ) : (
+  //     <>
+  //       <li>
+  //         <OpenModalButton
+  //           buttonText="Log In"
+  //           modalComponent={<LoginFormModal />}
+  //         />
+  //         {/* <NavLink to="/login">Log In</NavLink> */}
+  //       </li>
+  //       <li>
+  //         <OpenModalButton
+  //           buttonText="Sign Up"
+  //           modalComponent={<SignupFormModal />}
+  //         />
+  //         {/* <NavLink to="/signup">Sign Up</NavLink> */}
+  //       </li>
+  //     </>
+  //   );
+
+  return (
+    // <nav className='navigation-bar'>
+    //   <div className='logo'>
+    //     <NavLink to='/'>Home</NavLink>
+    //   </div>
+    //   <ul className='session-links'>
+    //     {isLoaded && sessionLinks}
+    //   </ul>
+    // </nav>
+    <nav>
+      <Link className='logo' to='/'>Fortbnb</Link>
+      { isLoaded && (sessionUser ? (
+        <div className='logged-in'>
+          <Link to='/spots'>Create a New Spot</Link>
+          <ProfileButton user={sessionUser} />
+        </div>
+      ) : (
+        <div>
           <OpenModalButton
             buttonText="Log In"
             modalComponent={<LoginFormModal />}
           />
-          {/* <NavLink to="/login">Log In</NavLink> */}
-        </li>
-        <li>
           <OpenModalButton
             buttonText="Sign Up"
             modalComponent={<SignupFormModal />}
           />
-          {/* <NavLink to="/signup">Sign Up</NavLink> */}
-        </li>
-      </>
-    );
-
-  return (
-    <nav className='navigation-bar'>
-      <div className='logo'>
-        <NavLink to='/'>Home</NavLink>
-      </div>
-      <ul className='session-links'>
-        {isLoaded && sessionLinks}
-      </ul>
+        </div>
+      ))}
     </nav>
     // <ul>
     //   <li>
