319addbcf866cef4156a78ae5dbaa757f5b0c6b5
Alex <gohalexx@gmail.com>
2024-04-22 08:12:43 -0700

    Home component test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>Welcome!</h1>
+        element: <Home />
       },
       // {
       //   path: 'login',
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <h1>Test title</h1>
+        </div>
+    )
+}
+
+export default Home;
