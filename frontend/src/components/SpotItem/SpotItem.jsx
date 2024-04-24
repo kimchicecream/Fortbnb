981633b87cd6c1659760cb7d833a154d6f79dbbe
Alex <gohalexx@gmail.com>
2024-04-24 04:49:09 -0700

    did some css

--- frontend/src/components/SpotItem/SpotItem.jsx
+++ frontend/src/components/SpotItem/SpotItem.jsx
@@ -8,18 +8,40 @@ function SpotItem({ spot }) {
         navigate(`/spots/${spotId}`);
     };
 
+    const displayRating = () => {
+        if (spot.avgRating !== null) {
+            return <p className='avg-rating'>{spot.avgRating}</p>
+        } else {
+            return <p className='avg-rating new'>NEW</p>
+        }
+    }
+
+    const displayStar = () => {
+        if (spot.avgRating > 4.6) {
+            return <img className='star' src='../../../public/stars/4.6above.png' />
+        } else if (spot.avgRating < 4.5 && spot.avgRating > 3.6) {
+            return <img className='star' src='../../../public/stars/4.5below.png' />
+        } else if (spot.avgRating < 3.5 && spot.avgRating > 2.6) {
+            return <img className='star' src='../../../public/stars/3.5below.png' />
+        } else if (spot.avgRating < 2.5 && spot.avgRating > 1.6) {
+            return <img className='star' src='../../../public/stars/2.5below.png' />
+        } else if (spot.avgRating > 0) {
+            return <img className='star' src='../../../public/stars/1.5below.png' />
+        }
+    }
+
     return (
-        <div className='spotitem-container'>
+        <div className='spotitem-container' title={`${spot.name}`}>
             <div className='image-container' onClick={() => onClick(spot.id)}>
-                {spot.previewImage}
+                <img className='image' src={spot.previewImage} alt={`Preview of ${spot.name}`} />
+                <div className='rating-overlay'>
+                    {displayStar()}
+                    <p className='rating'>{displayRating()}</p>
+                </div>
             </div>
             <div className='info-container' onClick={() => onClick(spot.id)}>
-                <h3 className='city-state'>{spot.city}, {spot.state}</h3>
+                <p className='city-state'>{spot.city}, {spot.state}</p>
                 <p className='price'>{spot.price} per night</p>
-                <div className='stars-container'>
-                    <p className='avg-stars'>{spot.avgRating}</p>
-                </div>
-
             </div>
         </div>
     )
