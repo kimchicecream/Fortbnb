d9f780b0404077da63f3f1c33104643283b2ab95
Alex <gohalexx@gmail.com>
2024-04-29 07:41:23 -0700

    fixed spot

--- backend/db/seeders/20240408092016-SpotImage.js
+++ backend/db/seeders/20240408092016-SpotImage.js
@@ -19,22 +19,22 @@ module.exports = {
       {
         spotId: 1,
         url: 'https://i.postimg.cc/1RjFvNMp/Unmoored-Manor-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 1,
         url: 'https://i.postimg.cc/W4wr8Wf3/Unmoored-Manor-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 1,
         url: 'https://i.postimg.cc/qqSCq9gm/Unmoored-Manor-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 1,
         url: 'https://i.postimg.cc/HncVqB3b/Unmoored-Manor-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 2,
@@ -44,22 +44,22 @@ module.exports = {
       {
         spotId: 2,
         url: 'https://i.postimg.cc/vHnJ0k5b/Seaside-Villa-1.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 2,
         url: 'https://i.postimg.cc/DyfG6k9b/Seaside-Villa-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 2,
         url: 'https://i.postimg.cc/QdycV4nK/Seaside-Villa-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 2,
         url: 'https://i.postimg.cc/R0JKt7js/Seaside-Villa-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 3,
@@ -69,22 +69,22 @@ module.exports = {
       {
         spotId: 3,
         url: 'https://i.postimg.cc/k5kTkXQ4/Mount-Olympus-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 3,
         url: 'https://i.postimg.cc/kggYSHMn/Mount-Olympus-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 3,
         url: 'https://i.postimg.cc/Mp3PmYX1/Mount-Olympus-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 3,
         url: 'https://i.postimg.cc/50Ls1dgm/Mount-Olympus-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 4,
@@ -94,22 +94,22 @@ module.exports = {
       {
         spotId: 4,
         url: 'https://i.postimg.cc/tJyGGz3Y/Cloistered-Castle-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 4,
         url: 'https://i.postimg.cc/pdwtqvyQ/Cloistered-Castle-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 4,
         url: 'https://i.postimg.cc/v8LdLpy9/Cloistered-Castle-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 4,
         url: 'https://i.postimg.cc/kXsq9Lh9/Cloistered-Castle-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 5,
@@ -119,22 +119,22 @@ module.exports = {
       {
         spotId: 5,
         url: 'https://i.postimg.cc/90pdLWXr/Apt-Reckless-Railways-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 5,
         url: 'https://i.postimg.cc/Hszz2tjB/Apt-Reckless-Railways-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 5,
         url: 'https://i.postimg.cc/QM2Jt4nx/Apt-Reckless-Railways-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 5,
         url: 'https://i.postimg.cc/jqQ4xh6t/Apt-Reckless-Railways-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 6,
@@ -144,22 +144,22 @@ module.exports = {
       {
         spotId: 6,
         url: 'https://i.postimg.cc/59RkvVKn/Beachside-Lodge-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 6,
         url: 'https://i.postimg.cc/bN4WXhQs/Beachside-Lodge-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 6,
         url: 'https://i.postimg.cc/TYSMRmX2/Beachside-Lodge-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 6,
         url: 'https://i.postimg.cc/XJ21sSjN/Beachside-Lodge-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 7,
@@ -169,22 +169,22 @@ module.exports = {
       {
         spotId: 7,
         url: 'https://i.postimg.cc/NM4FVDSj/The-Ol-Mill-House-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 7,
         url: 'https://i.postimg.cc/jSwW5zwM/The-Ol-Mill-House-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 7,
         url: 'https://i.postimg.cc/15YnsWtg/The-Ol-Mill-House-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 7,
         url: 'https://i.postimg.cc/j5rnBLjZ/The-Ol-Mill-House-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 8,
@@ -194,22 +194,22 @@ module.exports = {
       {
         spotId: 8,
         url: 'https://i.postimg.cc/Xv9kZMG1/Single-Room-Grand-Glacier-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 8,
         url: 'https://i.postimg.cc/L8cV64XZ/Single-Room-Grand-Glacier-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 8,
         url: 'https://i.postimg.cc/zGbjrHJr/Single-Room-Grand-Glacier-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 8,
         url: 'https://i.postimg.cc/Vsg4Bc73/Single-Room-Grand-Glacier-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 9,
@@ -219,22 +219,22 @@ module.exports = {
       {
         spotId: 9,
         url: 'https://i.postimg.cc/9QbqsW9h/Apt-Snooty-Steppes-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 9,
         url: 'https://i.postimg.cc/7Zw2BCf9/Apt-Snooty-Steppes-5.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 9,
         url: 'https://i.postimg.cc/6QQzmbyM/Apt-Snooty-Steppes-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 9,
         url: 'https://i.postimg.cc/GmPh5zZT/Apt-Snooty-Steppes-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 10,
@@ -244,22 +244,22 @@ module.exports = {
       {
         spotId: 10,
         url: 'https://i.postimg.cc/JzgVKXhH/Cabin-Zenith-Wall-2.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 10,
         url: 'https://i.postimg.cc/4Nfg3ZMy/Cabin-Zenith-Wall-3.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 10,
         url: 'https://i.postimg.cc/V68hHWPv/Cabin-Zenith-Wall-4.png',
-        preview: true
+        preview: false
       },
       {
         spotId: 10,
         url: 'https://i.postimg.cc/yN95Pr52/Cabin-Zenith-Wall-5.png',
-        preview: true
+        preview: false
       },
     ])
   },
